1952c709d56e1f143acaeca8ee75b7a8c0b769c6
barbourians <github@barbourians.org>
2017-04-28 14:46:56 +0200

    Added solution for 14-build-confusion-matrix.py

--- MLND/2-machine-learning-foundations/14-evaluation-metrics/14-build-confusion-matrix.py
+++ MLND/2-machine-learning-foundations/14-evaluation-metrics/14-build-confusion-matrix.py
@@ -1,60 +1,55 @@
-#
-# In this and the following exercises, you'll be adding train test splits to the data
-# to see how it changes the performance of each classifier
-#
-# The code provided will load the Titanic dataset like you did in project 0, then train
-# a decision tree (the method you used in your project) and a Bayesian classifier (as
-# discussed in the introduction videos). You don't need to worry about how these work for
-# now. 
-#
-# What you do need to do is import a train/test split, train the classifiers on the
-# training data, and store the resulting accuracy scores in the dictionary provided.
+# 14-build-confusion-matrix.py
+# In this exercise, we'll use the Titanic dataset as before, train two classifiers and
+# look at their confusion matrices. Your job is to create a train/test split in the data
+# and report the results in the dictionary at the bottom.
 
 import numpy as np
 import pandas as pd
 
 # Load the dataset
 print ("# Load the dataset ...")
+from sklearn import datasets
+#X = pd.read_csv('titanic_data.csv')
 X = pd.read_csv('\\Udacity\\machine-learning\\projects\\titanic_survival_exploration\\titanic_data.csv')
 
 # Limit to numeric data
 print ("# Limit to numeric data ...")
 X = X._get_numeric_data()
-
-# Separate the labels
 y = X['Survived']
-# Remove labels from the inputs, and age due to missing data
 del X['Age'], X['Survived']
 
 print ("# from sklearn import ...")
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 from sklearn.naive_bayes import GaussianNB
 from sklearn import cross_validation
 
 # TODO: split the data into training and testing sets,
-# using the standard settings for train_test_split.
+# using the default settings for train_test_split (or test_size = 0.25 if specified).
 # Then, train and test the classifiers with your newly split data instead of X and y.
 print ("# split the data into training and testing sets ...")
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.4, random_state=0)
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.25, random_state=0)
 
 # The decision tree classifier
 print ("# The decision tree classifier ...")
 clf1 = DecisionTreeClassifier()
 clf1.fit(X_train,y_train)
-answerDTC = accuracy_score(y_test, clf1.predict(X_test))
-print ("Decision Tree has accuracy: ",answerDTC)
+answerDTC = confusion_matrix(y_test,clf1.predict(X_test))
+print "Confusion matrix for this Decision Tree:\n",answerDTC
 
 # The naive Bayes classifier
 print ("# The naive Bayes classifier ...")
 clf2 = GaussianNB()
 clf2.fit(X_train,y_train)
-answerNBC = accuracy_score(y_test, clf2.predict(X_test))
-print ("GaussianNB has accuracy: ",answerNBC)
+answerNBC = confusion_matrix(y_test,clf2.predict(X_test))
+print "GaussianNB confusion matrix:\n",answerNBC
 
-answer = { 
- "Naive Bayes Score": answerNBC, 
- "Decision Tree Score": answerDTC
+#TODO: store the confusion matrices on the test sets below
+
+confusions = {
+ "Naive Bayes": answerNBC,
+ "Decision Tree": answerDTC
 }
 
+# EOF
 print ("EOF")
