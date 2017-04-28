64d66a7c5ad55a5d33e411d61c06f2ae22c9fcf6
barbourians <github@barbourians.org>
2017-04-28 14:18:45 +0200

    train and test the classifiers

--- naive_bayes_GaussianNB.py
+++ naive_bayes_GaussianNB.py
@@ -3,12 +3,25 @@ import numpy as np
 X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
 Y = np.array([1, 1, 1, 2, 2, 2])
 
+# Import statement
 from sklearn.naive_bayes import GaussianNB
 
+# Create the Classifier
 clf = GaussianNB()
-clf.fit(X, Y)
-print(clf.predict([[-0.8, -1]]))
 
+# Fit iy using X and Y
+clf.fit(X, Y)
+
+# Create a vector of predictions
+pred = clf.predict([[-0.8, -1]])
+print (pred)
+
+# Create the Classifier
 clf_pf = GaussianNB()
+
+# Fit iy using X and Y and uique(?)
 clf_pf.partial_fit(X, Y, np.unique(Y))
-print(clf_pf.predict([[-0.8, -1]]))
+
+# Create a vector of predictions
+pred = clf.predict([[-0.8, -1]])
+print (pred)
